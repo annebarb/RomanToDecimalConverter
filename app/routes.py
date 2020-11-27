fda3d2114d65075f57058cb7194b9d838ae3e3f3
Bogdan Surd <surd.bogdan@gmail.com>
2020-11-27 18:32:15 +0200

    I need help

--- app/routes.py
+++ app/routes.py
@@ -4,28 +4,24 @@ from app import app
 from functions.roman_to_decimal import *
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html', title='RomToDec Converter')
-
-
-@app.route('/', methods=['POST'])
-def index_post():
     if request.method == 'POST':
-        user_input = request.form['Input']
         try:
-            value = decimal_to_roman(int(user_input))
+            value = decimal_to_roman(int(request.form['Input']))
         except:
-            value = roman_to_decimal(user_input)
+            value = roman_to_decimal(request.form['Input'])
         print(value)
-    return render_template('index.html', title='RomToDec Converter')
+        return render_template('index.html', title='Converter', result=value)
+    else:
+        return render_template('index.html', title='Converter')
 
 
 @app.errorhandler(ValueError)
 def handle_value_error(err):
-    return render_template('index.html', title='RomToDec Converter')
+    return render_template('index.html', title='RomToDec Converter', message='Invalid value entered')
 
 
 @app.errorhandler(TypeError)
 def handle_value_error(err):
-    return render_template('index.html', title='RomToDec Converter')
+    return render_template('index.html', title='RomToDec Converter', message='Invalid value entered')
--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,13 +1,21 @@
-document.getElementById("Input")
-    .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("convertButton").click();
-    }
-});
+function enterPressed() {
+    document.getElementById('Input')
+        .addEventListener('keyup', function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById('convertButton').click();
+            document.getElementById('Output').textContent = "{{result}}";
+        }});
+}
 
-document.getElementById("convertButton").addEventListener("click", buttonClick);
 
 function buttonClick() {
-    document.getElementById("Output").value = "{{ value }}";
+    document.getElementById('Input')
+    document.getElementById('Output').textContent = "{{result}}";
 }
+
+function main() {
+  document.getElementById("convertButton").addEventListener("click", buttonClick);
+}
+
+document.addEventListener("DOMContentLoaded", main);
